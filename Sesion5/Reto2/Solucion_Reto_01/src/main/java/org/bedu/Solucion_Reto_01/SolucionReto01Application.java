fcdd45149987eceb0328e93eaab5a56d1f4528bf
Naomi Neriyah Casanova Rivera <132235823+Naomi-Casanova@users.noreply.github.com>
2025-06-22 21:09:24 -0600

    Reto 2 Sesion 5

---
+++ Sesion5/Reto2/Solucion_Reto_01/src/main/java/org/bedu/Solucion_Reto_01/SolucionReto01Application.java
@@ -0,0 +1,13 @@
+package org.bedu.Solucion_Reto_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucionReto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucionReto01Application.class, args);
+	}
+
+}
